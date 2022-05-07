2db9b9f8ad13d7699cfba91bdb38ddc5f8d73a69
fazilfazil1 <muhammed.fazil@neopraxis.in>
2022-05-07 13:16:54 +0530

    commit customer upload

--- djangoapp/views.py
+++ djangoapp/views.py
@@ -19,10 +19,8 @@ def upload_file(request):
     global a
     file = request.FILES['filefield']
     df = pd.read_excel(file, engine='openpyxl')
-    # df.to_excel('file.xlsx')
     a = df
     return render(request, 'dropdown.html', {'allcolumns': list(df.columns)})
-
 
 
 def read_file(request):
@@ -35,7 +33,7 @@ def read_file(request):
     address1 = request.POST['select-10']
     address_city = request.POST['select7']
     country_code = request.POST['select8']
-    address_fname = request.POST['select5']
+    addresses_first = request.POST['select5']
     address_lname = request.POST['select9']
     postal_code = request.POST['select-11']
     state_or_province = request.POST['select-12']
@@ -43,19 +41,25 @@ def read_file(request):
     for index, row in a.iterrows():
         payload = [{
             "company": row[company],
-            "first_name":row[first_name],
-            "last_name":row[last_name],
-            "phone":row[phone],
+            "first_name": row[first_name],
+            "last_name": row[last_name],
+            "phone": str(row[phone]),
             "email": row[email],
-            "address1":row[address1],
-            "address_city":row[address_city],
-            "country_code":row[country_code],
-            "address_fname":row[address_fname],
-            "address_lname":row[address_lname],
-            "postal_code":row[postal_code],
-            "state_or_province":row[state_or_province]
+            "addresses": [
+                  {
+                    "first_name": row[addresses_first],
+                    "city": row[address_city],
+                    "country_code": row[country_code],
+                    "last_name": row[address_lname],
+                    "address1": row[address1],
+                    "postal_code": str(row[postal_code]),
+                    "state_or_province": row[state_or_province],
+                  }
+                  ]
         }]
+
         print(payload)
+
         headers = {
             "Content-Type": "application/json",
             "Accept": "application/json",
@@ -63,5 +67,5 @@ def read_file(request):
         }
         response = requests.request("POST", url, json=payload, headers=headers)
         print(response.text)
-        return render(request, 'drop.html')
+    return render(request, 'drop.html')
 
